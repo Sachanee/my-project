e8bd58f9a4d44e9e8f8590cad975f9a69083921c
Sachanee Madhukala <120290210+Sachanee@users.noreply.github.com>
2023-10-15 22:18:08 +0530

    Update sum.py
    Rewrote the code to make it more concise.

--- sum.py
+++ sum.py
@@ -1,15 +1,13 @@
 def sum_even_numbers(start, end):
-    total = 0
-    for number in range(start, end + 1):
-        if number % 2 == 0:
-            total += number
-    return total
+    if start > end:
+        return "Invalid range: The start should be less than or equal to the end."
+    
+    even_numbers = [number for number in range(start, end + 1) if number % 2 == 0]
+    total = sum(even_numbers)
+    return f"The sum of even numbers from {start} to {end} is {total}"
 
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 
-if start > end:
-    print("Invalid range: The start should be less than or equal to the end.")
-else:
-    result = sum_even_numbers(start, end)
-    print(f"The sum of even numbers from {start} to {end} is {result}")
+result = sum_even_numbers(start, end)
+print(result)
